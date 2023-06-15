7137f3d70d74689602d43d5a805dd58764da940f
Jean Mayer <jean.mayer@ae.studio>
2023-06-15 09:18:16 -0300

    validations

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -52,12 +52,12 @@ export default function BlogPost() {
             </h2>
             <div className="mt-12 pb-12">
               <article
-                key={post.id}
+                key={post?.id}
                 className="flex max-w-xl flex-col items-start justify-between border-t pt-12 mb-12 cursor-pointer p-4"
               >
-                {post.fotoDeCapa?.url && (
+                {post?.fotoDeCapa?.url && (
                   <img
-                    src={post.fotoDeCapa.url}
+                    src={post?.fotoDeCapa?.url}
                     className="h-48 mb-5 rounded"
                   />
                 )}
@@ -66,17 +66,17 @@ export default function BlogPost() {
                 </div>
                 <div className="group relative">
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    {post.titulo}
+                    {post?.titulo}
                   </h3>
                   <p
                     className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"
-                    dangerouslySetInnerHTML={{ __html: post.conteudo.html }}
+                    dangerouslySetInnerHTML={{ __html: post?.conteudo?.html }}
                   ></p>
                 </div>
                 <div className="relative mt-8 flex items-center gap-x-4">
-                  {post.fotoAutor?.url && (
+                  {post?.fotoAutor?.url && (
                     <img
-                      src={post.fotoAutor.url}
+                      src={post?.fotoAutor?.url}
                       alt=""
                       className="h-10 w-10 rounded-full bg-gray-50"
                     />
@@ -84,7 +84,7 @@ export default function BlogPost() {
                   <div className="text-sm leading-6">
                     <p className="font-semibold text-gray-900">
                       <span className="absolute inset-0" />
-                      {post.autor}
+                      {post?.autor}
                     </p>
                     <p className="text-gray-600">Advogada</p>
                   </div>
